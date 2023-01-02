a6df5c9b8c6af6d724334a3a82f80aa4a111d610
AshiqueRS <ashiq.cse24@gmail.com>
2023-01-02 15:54:57 +0600

    salarytype

--- src/views/salary_type/SalaryTypeForm.js
+++ src/views/salary_type/SalaryTypeForm.js
@@ -1,10 +1,87 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage, FieldArray } from 'formik';
+import * as Yup from 'yup';
+//import TextError from './TextError';
+
+const initialValues = {
+        salaryType: [{name:''}]
+    }
+
+const onSubmit = values => {
+    console.log(values);
+}
 
 
-const SalaryTypeForm = () => {
+// const validationSchema = Yup.object({
+//     name: Yup.string().required('Required')
+// })
+const validationSchema = Yup.object().shape({
+    salaryType: Yup.array().of(Yup.object().shape({
+        name: Yup.string().required('Required')
+}))
+})
 
+function SalaryTypeForm(){
+    
+    //console.log('Form values', formik.values)
+
+    
     return (
-        <h1>Salary Type Form</h1>
+        <Formik 
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+        >
+            <Form>
+            {/* <div className="form-control"  >
+                <label htmlFor='name'>Salary Type</label>
+                <Field 
+                    type='text'  
+                    id='name' 
+                    name='name'
+                    placeholder='Enter Salary Type'
+                    />
+                <ErrorMessage name='name' component={TextError}/>
+            </div> */}
+            <div className='form-control'>
+                <label>Salary Type</label>
+                <FieldArray 
+                    name='salaryType'
+                    
+                >
+                    {
+                        (fieldArrayProps) => {
+                           console.log('fieldArrayProps', fieldArrayProps)
+                           const {push,remove, form,ErrorMessage} = fieldArrayProps
+                           const {values} = form
+                           const {salaryType} = values
+                           return (
+                            <div>
+                            {salaryType.map((salaryTypes,index) => (
+                                <div key={index}>
+                                <Field name={`salaryType[${index}].name`} type='text' placeholder='Enter Salary Type'/>
+                                
+                                {index > 0 && (
+                                    <button type='button' onClick={() => remove(index)}>{' '} - {' '}
+                                    </button>
+                                    
+                                )}
+                                {ErrorMessage.salaryType[index].name}
+                                <button type='button' onClick={() => push({name: ''})}> + </button>
+                                
+                                </div>
+                           ))}
+                           
+                           </div>
+                            )
+                    }}
+                </FieldArray>
+            </div>
+                <button type='submit'>Submit</button>
+            </Form>
+        </Formik>
     )
 }
+
 
 export default SalaryTypeForm;
