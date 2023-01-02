0c08caddcaa024615a7ef2f0b2b384d651e1c708
sujoy99 <scg.sujoy@gmail.com>
2023-01-02 08:54:31 +0600

    form scratch ongoing

--- src/views/salary_type/SalaryTypeAdd.js
+++ src/views/salary_type/SalaryTypeAdd.js
@@ -1,10 +1,38 @@
+import React from 'react'
+import { Formik, Form } from 'formik'
+import { Card } from "react-bootstrap";
+import ProgressBar from "react-topbar-progress-indicator";
+
 import SalaryTypeForm from './SalaryTypeForm'
+import {SalaryType} from './SalaryType';
+
+
+
 
 const SalaryTypeAdd = () => { 
 
+    const onSubmit = values => {
+        console.log('Form data', values)
+    }
+
+
     return (
-        <SalaryTypeForm />
-    );
+        <>
+            <Card border='white' className='table-wrapper table-responsive'>
+                <Card.Body>
+                    {/* {loading && <ProgressBar />} */}
+                    <Formik
+                        initialValues={SalaryType}
+                        validationSchema={SalaryType.validator()}
+                        onSubmit={onSubmit}>
+                        {(props) => {
+                            return <SalaryTypeForm formType="add" {...props} />;
+                        }}
+                    </Formik>
+                </Card.Body>
+            </Card>
+        </>
+    )
 }
 
 export default SalaryTypeAdd;
